41bbbf366f3846580b474933db376fb451c25ec8
Kristain86 <cristiansagula@gmail.com>
2019-04-02 17:50:26 -0300

    maquetado1

--- index.php
+++ index.php
@@ -5,8 +5,44 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Pokemon</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/custom.css">
 </head>
 <body>
-  
+
+  <header>
+  <div class="container">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png" alt="">
+  </div>
+  </header>
+
+  <section>
+    <article class="pokemon-cards">
+      <div class="container">
+      <h1>Pokemons:</h1>
+        <div class="row">
+
+            <div class="col-md-4">
+              <div class="pokemon-card">
+                <h3>Nombre pokemon</h3>
+                <p>Descripción: lorem ipsum lorem </p>
+                <img src="https://placehold.it/350x250" alt="">
+                <ul class="specs">
+                  <h4>Ataques:</h4>
+                  <li>Ataque 1</li>
+                  <li>Ataque 2</li>
+                </ul>
+                <h5>Se encuentra en la región de: <span><b> región </b></span></h5>
+              </div>
+            </div>
+        </div>
+      </div>
+    </article>
+  </section>
+
+
+
+
+  <script src="js/scripts.js"></script>
 </body>
 </html>
